0778caf8c582cc1c677dd4a0cfd952446e0ff237
xururuca9797 <xruruca-@naver.com>
2020-10-17 13:45:34 +0900

    [add] - storybook-static

--- src/pages/Gnb.js
+++ src/pages/Gnb.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import { StorybookIndex } from "../../storybook-static/index.html";
 
 export const Gnb = () => {
   return (
---
+++ storybook-static/precache-manifest.4b451ec0eae58422b7c95ea3ff29c5fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826d8dd839b4f4c47f4764a1c45b9f63",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.71901babeb5e9211efe8.bundle.js"
+  },
+  {
+    "url": "runtime~main.71901babeb5e9211efe8.bundle.js"
+  },
+  {
+    "url": "vendors~main.71901babeb5e9211efe8.bundle.js"
+  },
+  {
+    "url": "vendors~main.71901babeb5e9211efe8.bundle.js.LICENSE.txt"
+  }
+]);
