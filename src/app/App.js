d0b63e0dbc905dc6288361822a2c63b2fd8895d7
xururuca9797 <xruruca-@naver.com>
2020-10-17 13:24:33 +0900

    [update] - BrowserRouter

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Gnb, Main, Sub } from "../pages/Index";
 // import mainImg from "./main.jpg";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Gnb />
       <Route exact path="/" component={Main} />
       <Route exact path="/sub" component={Sub} />
-    </div>
+    </BrowserRouter>
   );
 }
 
