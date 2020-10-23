2f5ba82d0efa4fe8fcf2ee64b0637a332be21826
xururuca9797 <xruruca-@naver.com>
2020-10-24 00:44:27 +0900

    [setting] - FontAwesome

--- src/pages/gnb/Gnb.js
+++ src/pages/gnb/Gnb.js
@@ -1,109 +1,232 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import mainImg from "../../assets/image/main_logo.svg";
+
+// FontAwesomeIcon
+import {
+  faBars,
+  faSmile,
+  faDesktop,
+  faPalette,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Gnb = () => {
   return (
     <GnbWrapper>
-      <Headergnb>
-        <Headerlogo>
+      <GnbLogo>
+        <Link to="/home">
           <img src={mainImg} alt="메인로고" />
-        </Headerlogo>
-        <Headerlist>
-          <Headeritem>
-            <Link to="/about">About</Link>
-          </Headeritem>
-          <Headeritem>
-            <Link to="/develop">Develop</Link>
-          </Headeritem>
-          <Headeritem>
-            <Link to="/design">Design</Link>
-          </Headeritem>
-          <Headeritem>
-            <Link to="/contact">Contact</Link>
-          </Headeritem>
-        </Headerlist>
-      </Headergnb>
+        </Link>
+      </GnbLogo>
+      <GnbHam>
+        <GnbStyledFontAwesomeIcon icon={faBars} size="2x" />
+      </GnbHam>
+      <GnbInner>
+        <GnbList>
+          <li>
+            <Link to="/about">
+              <FontAwesomeIcon icon={faSmile} size="m" />
+              about
+            </Link>
+          </li>
+          <li>
+            <Link to="/develop">
+              <FontAwesomeIcon icon={faDesktop} size="m" />
+              develop
+            </Link>
+            <GnbSub>
+              <li>
+                <Link to="/develop">work</Link>
+              </li>
+              <li>
+                <Link to="/component">component</Link>
+              </li>
+              <li>
+                <Link to="/study">study</Link>
+              </li>
+            </GnbSub>
+          </li>
+          <li>
+            <Link to="/design">
+              <FontAwesomeIcon icon={faPalette} size="m" />
+              design
+            </Link>
+            <GnbSub>
+              <li>
+                <Link to="/graphics">web_graphics</Link>
+              </li>
+              <li>
+                <Link to="/artwork">artwork</Link>
+              </li>
+            </GnbSub>
+          </li>
+          <li>
+            <Link to="/contact">
+              <FontAwesomeIcon icon={faEnvelope} size="m" />
+              contact
+            </Link>
+          </li>
+        </GnbList>
+      </GnbInner>
+      <GnbTab>
+        <GnbTabList>
+          <li>
+            <Link to="/aboutl">
+              <i class="fas fa-smile"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/develop">
+              <i class="fas fa-desktop"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/design">
+              <i class="fas fa-palette"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              <i class="fas fa-envelope"></i>
+            </Link>
+          </li>
+        </GnbTabList>
+      </GnbTab>
     </GnbWrapper>
   );
 };
 
 const GnbWrapper = styled.header`
-  width: 100%;
-  z-index: 99;
-  position: absolute;
-  animation: header 4s cubic-bezier(0.19, 1, 0.22, 1);
-  > a {
-    float: left;
-    width: 25%;
-  }
-  @keyframes header {
-    0% {
-      opacity: 0;
-      transform: translateY(-200px);
-    }
-    50% {
-      opacity: 0;
-      transform: translateY(-100px);
-    }
-    100% {
-      opacity: 1;
-      transform: translateY(0px);
-    }
-  }
+  width: 200px;
+  height: 100%;
+  position: fixed;
+  transition: 0.5;
+  overflow: hidden;
+  z-index: 100;
 `;
-const Headergnb = styled.nav`
-  max-width: 1280px;
-  margin: auto;
-  display: flex;
-  justify-content: space-around;
-`;
-const Headerlogo = styled.h1`
-  > img {
-    width: 140px;
-    padding-top: 5px;
+
+const GnbLogo = styled.h1`
+  width: 180px;
+  margin: 20px 10px 30px 10px;
+  > a > img {
+    width: 100%;
   }
 `;
 
-const Headerlist = styled.ul`
-  justify-content: inherit;
-  display: flex;
-  align-items: center;
-  list-style: none;
-  width: 1140px;
-  padding-left: 0px;
-`;
-const Headeritem = styled.li`
-  position: relative;
-  width: 25%;
+const GnbInner = styled.div`
+  width: 200px;
   height: 100%;
-  text-align: center;
-  align-items: center;
-  > a::before {
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-sizing: border-box;
+  border-top-right-radius: 40px;
+  border-bottom-right-radius: 40px;
+  transition: 0.5;
+  overflow: hidden;
+`;
+
+const GnbHam = styled.button`
+  width: 45px;
+  height: 45px;
+  background-color: #fff;
+  border-radius: 20%;
+  border: none;
+  left: 20px;
+  top: 120px;
+  position: absolute;
+  z-index: 100;
+  cursor: pointer;
+  border: none;
+  outline: none;
+`;
+const GnbList = styled.ul`
+  width: 200px;
+  font-size: 20px;
+  padding-top: 80px;
+  font-family: "NanumSquare", sans-serif !important;
+  > li {
     width: 100%;
-    height: 6px;
-    content: "";
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: #ede6d3;
-    transition: 0.2s cubic-bezier(0.47, 0, 0.75, 0.72);
+    text-transform: capitalize;
+    position: relative;
+    overflow: hidden;
+    transition: 0.5s;
   }
-  :hover > a {
-    color: #4271e9;
-  }
-  > a {
-    font-weight: 600;
+  > li > a {
     display: block;
-    width: 100%;
-    height: 100%;
-    text-decoration: none;
+    margin: 10px 10px;
+    padding: 20px 30px;
+    border-radius: 10px;
+    transition: 0.5s;
+    font-weight: bold;
+    color: rgb(62, 65, 73);
+  }
+  > li > a:hover {
+    background-color: var(--blue);
     color: #fff;
-    line-height: 95px;
   }
-  :hover > a::before {
-    height: 100%;
+  > li > a svg {
+    margin-right: 10px;
+  }
+`;
+const GnbSub = styled.ul`
+  & > li {
+    padding-left: 40px;
+    width: 100%;
+    font-size: 17px;
+    line-height: 35px;
+    font-weight: bold;
+  }
+  & > li > a {
+    color: #6b789c;
+    display: block;
+    transition: 0.5s;
+  }
+  & > li > a:hover {
+    color: #081435;
+    text-decoration: underline;
+  }
+`;
+const GnbStyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  color: red;
+`;
+
+// Tab메뉴 서식
+const GnbTab = styled.div`
+  width: 95px;
+  height: 100%;
+  position: fixed;
+  box-sizing: border-box;
+  transition: 0.5;
+  padding: 0px 20px;
+  background-color: var(--blue);
+  border-top-right-radius: 40px;
+  left: -300%;
+  top: 130px;
+`;
+const GnbTabList = styled.ul`
+  padding-top: 130px;
+  > li {
+    width: 45px;
+    height: 45px;
+    margin-bottom: 40px;
+    background-color: #fff;
+    border-radius: 20%;
+  }
+  > li:hover {
+    background-color: rgb(230, 230, 230);
+  }
+  > li > a {
+    display: block;
+    transition: 0.5s;
+    font-size: 25px;
+    color: var(--blue);
+    text-align: center;
+    line-height: 43px;
+  }
+  > li:hover a {
+    color: var(--pink);
   }
 `;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,9 +2,68 @@ import React from "react";
 import styled from "styled-components";
 
 export const Home = () => {
-  return <HomeWrapper>홈페이지</HomeWrapper>;
+  return (
+    <HomeWrapper>
+      <HomeVisual>
+        {/* 스크롤 효과 */}
+        <CollapseBgWrap>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </CollapseBgWrap>
+        <HomeGrid>
+          <GridItem className="gline01"></GridItem>
+          <GridItem className="gline02"></GridItem>
+          <GridItem className="gline03"></GridItem>
+          <GridItem className="gline04"></GridItem>
+          <GridItem className="gline05"></GridItem>
+        </HomeGrid>
+      </HomeVisual>
+    </HomeWrapper>
+  );
 };
 
-const HomeWrapper = styled.div`
-  background: green;
+const HomeWrapper = styled.main`
+  position: relative;
+  width: 100%;
+  // position: relative;
+  // width: 100%;
+  // height: 100vh;
+  // overflow: hidden;
+  // z-index: 15;
+`;
+const HomeVisual = styled.section`
+  height: 500px;
+  min-height: 100vh;
+  z-index: 3;
+  overflow: hidden;
+  background-color: #001842;
+`;
+
+// 스크롤 효과
+const CollapseBgWrap = styled.div`
+  border: 1px solid red;
+`;
+const HomeGrid = styled.div`
+  top: 0;
+  height: 100%;
+  width: inherit;
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+`;
+const GridItem = styled.div`
+  width: 1px;
+  background: hsla(0, 0%, 100%, 0.1);
+  transform-origin: top;
+  /* transform: scale(0); 가운데로사라짐*/
+  transition: transform 2s linear;
+  height: 100%;
 `;
