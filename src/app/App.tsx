3b3ad3127392c7d455f7659172848f9f5b1eb06a
jinny(윤유진) <jinny.youn@dktechin.com>
2020-10-25 23:04:08 +0900

    [add] - typescript

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { Home, Gnb, About, Develop, Design, Contact } from "../pages/Index";
-// import mainImg from "./main.jpg";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Gnb />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/develop" component={Develop} />
       <Route exact path="/design" component={Design} />
       <Route exact path="/contact" component={Contact} />
-    </BrowserRouter>
+    </Router>
   );
 }
 
