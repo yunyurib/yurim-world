cd716262b55750659942e36369bdfd63a4ee96c6
xururuca9797 <xruruca-@naver.com>
2020-11-17 18:44:57 +0900

    [update] - design의 디테일, 배너, 이벤트 내용 추가

---
+++ src/pages/design/Design.data.ts
@@ -0,0 +1,85 @@
+export const designDetailData = [
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드명 : 타임 트래벌러",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#의류디자인 #상세페이지 #썸네일 #그래픽 #보정",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드명 : 타임 트래벌러",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#의류디자인 #상세페이지 #썸네일 #그래픽 #보정",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드명 : 타임 트래벌러",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#의류디자인 #상세페이지 #썸네일 #그래픽 #보정",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드명 : 타임 트래벌러",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#의류디자인 #상세페이지 #썸네일 #그래픽 #보정",
+        button:"Explore"
+    }
+];
+
+export const designEventItem = [
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "1월의 혜택",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#기획 #디자인 #그래픽 #업로드",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "1월의 혜택",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#기획 #디자인 #그래픽 #업로드",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "1월의 혜택",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#기획 #디자인 #그래픽 #업로드",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "1월의 혜택",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#기획 #디자인 #그래픽 #업로드",
+        button:"Explore"
+    }
+];
+
+export const designBannerItem = [
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드: 제멋",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#배너디자인 #적용 #sns",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드: 더블유브이",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#배너디자인 #적용 #sns",
+        button:"Explore"
+    },
+    {
+        href:"https://store.musinsa.com/app/product/detail/990945/0",
+        title: "브랜드: 페플",
+        src:" http://www.fairplay142.com/shopimages/efairplay/0170010081182.jpg?1564016507",
+        text:"#배너디자인 #적용 #sns",
+        button:"Explore"
+    }
+];
--- src/pages/design/Design.js
+++ src/pages/design/Design.js
@@ -1,47 +1,5 @@
-import React from "react";
-import styled from "styled-components";
-
-export const Design = () => {
-  return (
-    <DesignWrapper>
-      <DesignGrid />
-      <DesignCont>
-        <h2>DESIGN</h2>
-        <DesignContClone></DesignContClone>
-      </DesignCont>
-    </DesignWrapper>
-  );
-};
-
-const DesignWrapper = styled.div`
-  width: 100%;
-  background-color: #1e1e1e;
-`;
-const DesignGrid = styled.div`
-  top: 0;
-  left: 50%;
-  position: fixed;
-  width: 1px;
-  height: 100%;
-  background: hsla(0, 0%, 100%, 0.1);
-`;
-const DesignCont = styled.section`
-  z-index: 3;
-  background-color: #1e1e1e;
-  justify-content: center;
-  > h2 {
-    font-size: 12em;
-    font-family: "Squada One", cursive;
-    padding: 2vw 6.25vw 0vw 6.25vw;
-  }
-`;
-const DesignContClone = styled.div`
-  width: 100%;
-  padding: 0 6.25vw 0vw 6.25vw;
-  border-top: 1px solid hsla(0, 0%, 100%, 0.1);
-  border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
-  box-sizing: border-box;
-  display: flex;
-  transform: translateY(-60px);
-  transition: all 0.5s 0.1s ease-in;
-`;
+//js
+document.querySelector(".more").click(function () {
+  document.querySelector(".test").animate({ display: "block" });
+  document.querySelector(".more").animate({ display: "none" });
+});
