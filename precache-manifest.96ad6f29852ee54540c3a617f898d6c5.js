1b623fa9cb5eebfcecf90c1a31db50d65273d173
xururuca9797 <xruruca-@naver.com>
2020-11-25 19:04:19 +0900

    Updates

---
+++ precache-manifest.96ad6f29852ee54540c3a617f898d6c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6123095f7db3e4810ee1b23e8f004af",
+    "url": "/yurim-world/index.html"
+  },
+  {
+    "revision": "57ce0f924e791141d0d1",
+    "url": "/yurim-world/static/css/main.41b4133e.chunk.css"
+  },
+  {
+    "revision": "fcc69e3ef0c076d7aa12",
+    "url": "/yurim-world/static/js/2.f8fa681b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yurim-world/static/js/2.f8fa681b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ce0f924e791141d0d1",
+    "url": "/yurim-world/static/js/main.e6acb448.chunk.js"
+  },
+  {
+    "revision": "62d2e64640519209342e",
+    "url": "/yurim-world/static/js/runtime-main.17e24ea2.js"
+  },
+  {
+    "revision": "c77aa973d7a7d627e3150a6069b098df",
+    "url": "/yurim-world/static/media/about-skill-logos.c77aa973.png"
+  },
+  {
+    "revision": "2a25bf5ee8098dd49ba22b10a20be6a9",
+    "url": "/yurim-world/static/media/aboutcontleft_profile.2a25bf5e.jpg"
+  }
+]);
